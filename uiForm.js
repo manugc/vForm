c815bd7deae1bbf064f09d59b9056848924f629a
manugc <lagartogc@gmail.com>
2015-08-20 17:16:09 +0100

    Update uiForm.js

--- uiForm.js
+++ uiForm.js
@@ -1,59 +1,59 @@
 (function (global, undefined) {
-	var $ = function (control) {
-		if ( global === this ) {
-			return new $(control);
+    var $ = function (control) {
+        if (global === this) {
+            return new $(control);
         }
-		
-		// Si se ha pasado un nombre, asignamos el objecto
-		if(control != undefined){
-			$.control = theRoot.dataView().control(control);
-		}
-		
-		$.frm = theRoot.dataView();
-		$.frmObj = theRoot.objectInfo();
-		
-		// constructor code goes here
+
+        // Si se ha pasado un nombre, asignamos el objecto
+        if (control != undefined) {
+            $.control = theRoot.dataView().control(control);
+        }
+
+        $.frm = theRoot.dataView();
+        $.frmObj = theRoot.objectInfo();
+
+        // constructor code goes here
         return this;
     };
-	
-	$.fn = $.prototype = {
-		control: function(){
-			return $.control
-		},
-		objecto: function(){
-			return $.frmObj.subObjectInfo(19, $.control.objectName);
-		},
-		getFocus: function(){
-			var count = $.frm.controlCount();
-			var getControl = theRoot.dataView().control;					
-			for(var i=0;i<count;i++){
-				try{
-					var controlFrm = getControl(i);					
-					if( ("focus" in controlFrm) && controlFrm.focus ){
-						$.control = controlFrm;
-						return controlFrm;
-					}
-				}catch(e){}
-			}
-		},
-		comboFillWithStaticTable: function(tablaEstatica){
-			var combo = $.control;
-			if(theMainWindow.widgetType(combo) === VMainWindow.WTypeComboBox){		
-				for(var i=0, size=theApp.staticTableItemCount(tablaEstatica); i<size; i++){
-					icono = theApp.staticTableItemImage(tablaEstatica, i);
-					texto = theApp.staticTableItemName(tablaEstatica, i);
-					id = theApp.staticTableItemId(tablaEstatica, i);
-					
-					if(icono === null){				
-						combo.addItem(texto, id);
-					}else{
-						combo.addItem(icono, texto, id);
-					}
-				}
-			}
-			return this;
-		}
-	};
+
+    $.fn = $.prototype = {
+        control: function () {
+            return $.control
+        },
+        objecto: function () {
+            return $.frmObj.subObjectInfo(19, $.control.objectName);
+        },
+        getFocus: function () {
+            var count = $.frm.controlCount();
+            var getControl = theRoot.dataView().control;
+            for (var i = 0; i < count; i++) {
+                try {
+                    var controlFrm = getControl(i);
+                    if (("focus" in controlFrm) && controlFrm.focus) {
+                        $.control = controlFrm;
+                        return controlFrm;
+                    }
+                } catch (e) {}
+            }
+        },
+        comboFillWithStaticTable: function (tablaEstatica) {
+            var combo = $.control;
+            if (theMainWindow.widgetType(combo) === VMainWindow.WTypeComboBox) {
+                for (var i = 0, size = theApp.staticTableItemCount(tablaEstatica); i < size; i++) {
+                    icono = theApp.staticTableItemImage(tablaEstatica, i);
+                    texto = theApp.staticTableItemName(tablaEstatica, i);
+                    id = theApp.staticTableItemId(tablaEstatica, i);
+
+                    if (icono === null) {
+                        combo.addItem(texto, id);
+                    } else {
+                        combo.addItem(icono, texto, id);
+                    }
+                }
+            }
+            return this;
+        }
+    };
 
     global.$ = $;
 })(this);
